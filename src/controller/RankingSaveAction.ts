0160a0a19e130e49e79021ae62797899d56efd6a
HawkEyyeAce <elodie.grosmaire@hotmail.fr>
2020-09-13 12:29:18 +0200

    Add update score feature

--- src/controller/RankingSaveAction.ts
+++ src/controller/RankingSaveAction.ts
@@ -7,22 +7,51 @@ import { Ranking } from "../entity/Ranking";
  */
 export async function rankingSaveAction(request: Request, response: Response) {
 
+    let error = false;
+
     // get a ranking repository to perform operations with ranking
     const rankingRepository = getManager().getRepository(Ranking);
 
-    // create a real ranking object from ranking json object sent over http
-    const newRanking = rankingRepository.create(request.body);
+    let userData = await rankingRepository.find({ where: {"userID": request.body.userID}});
+    console.log("userData", userData);
 
-    let data = {
-        'userID': request.body.userID,
-        'score': request.body.score
-    };
+    // check if this userID is already in the database
+    if (Array.isArray(userData) && userData.length)
+    {
+        console.log("Try to update userID");
+        if (userData.length == 1)
+        {
+            const ranking = rankingRepository.merge(userData[0], {userID: request.body.userID}, {score: request.body.score});
+            rankingRepository.save(ranking);
+        }
+        else
+        {
+            console.log("Too many userID found in database.");
+            error = true;
+            response.send("Error: Too many userID found in database.");
+        }
+    }
+    else
+    {
+        // create a real ranking object from ranking json object sent over http
+        const newRanking = rankingRepository.create(request.body);
 
-    console.log(new Date().toUTCString() + " / " + request.method + " / " + rankingSaveAction.name + " / Data sent : " + JSON.stringify(data) + "\n");
+        // save received ranking
+        await rankingRepository.save(newRanking);
 
-    // save received ranking
-    await rankingRepository.save(newRanking);
+        console.log("Create new userID");
+    }
     
-    // return saved ranking back
-    response.send("Score sent");
+    if (!error)
+    {
+        let data = {
+            'userID': request.body.userID,
+            'score': request.body.score
+        };
+
+        console.log(new Date().toUTCString() + " / " + request.method + " / " + rankingSaveAction.name + " / Data sent : " + JSON.stringify(data) + "\n");
+
+        // return saved ranking back
+        response.send("Score sent");
+    }
 }
